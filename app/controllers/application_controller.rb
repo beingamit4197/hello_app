ec1c679c09a16fe9ad5e945f1f21a8654f7b0295
Amit Rawat <beingamit4197@gmail.com>
2019-08-28 13:27:27 +0530

    name change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def name
-    render html: "hello : Amit"
+    render html: "hello : rawat"
   end           
 
   def amit
