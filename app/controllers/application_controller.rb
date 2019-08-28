9edd9a32940cff386e6ae8750af63658a480ec05
Amit Rawat <beingamit4197@gmail.com>
2019-08-28 13:33:38 +0530

    Page heading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def name
-    render html: "hello : rawat"
+    render html: "Welcome to my new web page.."
   end           
 
   def amit
